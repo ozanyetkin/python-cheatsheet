f115f294a27725e8a2e2608e10d26a065b11f9c0
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2022-04-30 16:07:09 +0300

    operators added

---
+++ syntax/operators.py
@@ -0,0 +1,16 @@
+num_1 = 10
+num_2 = 3
+
+summation = num_1 + num_2
+subtraction = num_1 - num_2
+multiplication = num_1 * num_2
+division = num_1 / num_2
+modulo = num_1 % num_2
+exponentiation = num_1 ** num_2
+
+print(summation)
+print(subtraction)
+print(multiplication)
+print(division)
+print(modulo)
+print(exponentiation)
