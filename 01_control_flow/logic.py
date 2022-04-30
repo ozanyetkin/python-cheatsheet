82d323d7c1b90cf52624b7c74cd269f79852878f
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2022-04-30 16:54:52 +0300

    logic finished

---
+++ 01_control_flow/logic.py
@@ -0,0 +1,9 @@
+True and True     # Evaluates to True
+True and False    # Evaluates to False
+False and False   # Evaluates to False
+1 == 1 and 1 < 2  # Evaluates to True
+1 < 2 and 3 < 1   # Evaluates to False
+"Yes" and 100     # Evaluates to 100
+"Yes" and 0       # Evaluates to 0
+
+print(True and True or 1==1 and 2 > 3 or True)
---
+++ example.py
@@ -0,0 +1,7 @@
+num_1 = 10
+num_2 = 5
+
+summation = num_1 + num_2
+subtraction = num_1 - num_2
+
+print(summation >= subtraction)
