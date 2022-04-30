30e52974c561baf62d0853789af1e0ed69bea2d6
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2022-04-30 15:46:08 +0300

    variables explained and added in varibles.py

---
+++ syntax/variables.py
@@ -0,0 +1,11 @@
+an_integer = 5
+a_float = 3.2
+a_string = "hello"
+another_string = " world"
+a_boolean = True
+another_boolean = False
+
+print(an_integer + a_float)
+print(a_string + another_string)
+print(an_integer + another_boolean)
+print(str(a_float) + another_string)
