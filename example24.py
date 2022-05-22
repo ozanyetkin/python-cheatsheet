6f92c7d8364b0fab70c5b0482c8ba9a6ae4419b0
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2022-05-22 15:25:38 +0300

    maze continues

--- example24.py
+++ example24.py
@@ -1,3 +1,4 @@
+import numpy as np
 # Initialize a maze that is solvable
 maze1 = [
     [0, 1, 1, 1, 0, 0, 0, 1],
@@ -35,13 +36,35 @@ maze3 = [
     [0, 1, 1, 0, 1, 0, 1, 1, 0],
 ]
 
+def check_pos(konum, maze):
+    if konum == (0, 0) or konum == (0, len(maze[0]) - 1) or konum == (len(maze) - 1, 0) or konum == (len(maze) - 1, len(maze[-1]) - 1):
+        return "kose"
+    elif konum[0] == 0 or konum[0] == len(maze) - 1 or konum[1] == 0 or konum[1] == len(maze[0]) - 1:
+        return "kenar"
+    else:
+        return "orta"
+
+def maze_tra(maze):
+    satir = [1] * len(maze[0])
+    new_maze = [satir] + maze + [satir]
+    for i in range(len(new_maze)):
+        new_maze[i] = [1] + new_maze[i] + [1]
+    return new_maze
+
+def next_step():
+    pass
+
+# print(check_pos((7,7), maze1))
+# print(check_pos((0,7), maze1))
+# print(check_pos((2,0), maze1))
+# print(check_pos((3,4), maze1))
 
 # Test cases for solvable and not solvable mazes
-print(is_solvable(maze1, (0, 0), (7, 7)))
+# print(is_solvable(maze1, (0, 0), (7, 7)))
 # should print True
 
-print(is_solvable(maze2, (0, 0), (7, 7)))
+# print(is_solvable(maze2, (0, 0), (7, 7)))
 # should print False
 
-print(is_solvable(maze3, (0, 0), (8, 8)))
+# print(is_solvable(maze3, (0, 0), (8, 8)))
 # should print True
