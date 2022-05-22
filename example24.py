44d8560444c3dea73a462d1dc4404a5360f43b8e
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2022-05-22 18:34:57 +0300

    class ended

--- example24.py
+++ example24.py
@@ -63,7 +63,7 @@ def next_step(konum, maze):
         if maze[i[0]][i[1]] == 0:
             aday.append(i)
     return aday
-
+"""
 def modif(koord, maze):
     k1 = koord[0]
     k2 = koord[1]
@@ -76,12 +76,12 @@ def modif(koord, maze):
     if koord[1] == len(maze[0]) - 1:
         k2 -= 1
     return (k1, k2)
-
-def run(konum, son, maze, path, crossover):
+"""
+def run(konum, son, maze, path=[], crossover=[], count=0):
     path.append(konum)
-    print(konum)
-    print(crossover)
-    print(np.array(maze))
+    # print(konum)
+    # print(crossover)
+    # print(np.array(maze))
     maze[konum[0]][konum[1]] = 1
     if konum == son:
         return True, path
@@ -93,6 +93,7 @@ def run(konum, son, maze, path, crossover):
             crossover = crossover[1:]
             return run(sonraki, son, maze, path, crossover)
     else:
+        count += 1
         sonraki = next_step(konum, maze)[0]
         kalanlar = next_step(konum, maze)[1:]
         if kalanlar != []:
@@ -100,10 +101,10 @@ def run(konum, son, maze, path, crossover):
         return run(sonraki, son, maze, path, crossover)
 
 def is_solvable(maze, bas, son):
+    maze = maze_tra(maze)
     bas = (bas[0] + 1, bas[1] + 1)
     son = (son[0] + 1, son[1] + 1)
-    maze = maze_tra(maze)
-    return run(bas, son, maze, [], [])
+    return run(bas, son, maze)
 
 # print(check_pos((7,7), maze1))
 # print(check_pos((0,7), maze1))
@@ -120,3 +121,39 @@ print(np.array(maze_tra(maze3)))
 
 # print(is_solvable(maze3, (0, 0), (8, 8)))
 # should print True
+
+# Initialize the function that takes maze, start, and goal as input
+def is_solvable(maze, start, goal):
+    path = set()
+    advance(start, maze, path)
+    if goal in path:
+        return True
+    else:
+        return False
+
+def get_neighbors(position, maze):
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    neighbors = [(position[0] + dir[0], position[1] + dir[1]) for dir in directions]
+    neighbors = list(filter(lambda p: 0 <= p[0] < len(maze[0]), neighbors))
+    neighbors = list(filter(lambda p: 0 <= p[1] < len(maze), neighbors))
+    return neighbors
+
+def get_value(position, maze):
+    return maze[position[1]][position[0]]
+
+def advance(position, maze, path):
+    if position in path or get_value(position, maze) == 1:
+        return
+    path.add(position)
+    for n in get_neighbors(position, maze):
+        advance(n, maze, path)
+
+print(is_solvable(maze1, (0, 0), (7, 7)))
+# should print True
+
+print(is_solvable(maze2, (0, 0), (7, 7)))
+# should print False
+
+print(is_solvable(maze3, (0, 0), (8, 8)))
+
+print(is_solvable(maze3, (0, 0), (8, 8)))
