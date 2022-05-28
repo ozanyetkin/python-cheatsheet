1119512a8e61e6d685d619610c28e6dc0ab5608c
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2022-05-28 19:05:38 +0300

    saturday class finished

--- example26.py
+++ example26.py
@@ -6,7 +6,7 @@ def read_file(file_name):
     return lines
 
 diabetes_data = read_file("diabetes_data")
-
+"""
 count = 0
 total = 0
 for i in range(1, len(diabetes_data)):
@@ -16,3 +16,45 @@ for i in range(1, len(diabetes_data)):
 
 average = total / count
 print(average)
+"""
+def gen_dict(file_name):
+    data = read_file(file_name)
+    column_num = len(data[0])
+    dictionary = {}
+    for i in range(column_num):
+        sutun = []
+        for j in range(1, len(data)):
+            sutun.append(data[j][i])
+        dictionary.update({data[0][i]: sutun})
+    return dictionary
+
+diabetes_dict = gen_dict("diabetes_data")
+print(diabetes_dict.keys())
+
+def find_na(dicti):
+    dicti_na = {}
+    for key in dicti.keys():
+        count_key = 0
+        for val in dicti[key]:
+            if val == "NA":
+                count_key += 1
+        dicti_na.update({key: count_key})
+    return dicti_na
+
+print(find_na(diabetes_dict))
+
+def find_min_max(lookup, minim, maxim, target, dicti):
+    target_min = 99999
+    target_max = 0
+    for i, val in enumerate(dicti[lookup]):
+        if val != "NA" and dicti[target][i] != "NA":
+            if maxim >= float(val) >= minim:
+                if float(dicti[target][i]) < target_min:
+                    target_min = float(dicti[target][i])
+                if float(dicti[target][i]) > target_max:
+                    target_max = float(dicti[target][i])
+    return (target_min, target_max)
+
+print(find_min_max("Age", 20, 30, "blood_pressure", diabetes_dict))
+print(find_min_max("Age", 20, 30, "serum_insulin", diabetes_dict))
+print(find_min_max("serum_insulin", 20, 50, "Age", diabetes_dict))
--- worksheet.py
+++ worksheet.py
@@ -1,10 +1,4 @@
-# glucose_concentration	blood_pressure	serum_insulin	Body_mass_index	Age
-# 148	72	NA	33.6	50
-# 85	66	NA	26.6	31
+a = 6
 
-dict_example = {
-    "glucose_concentration": 148,
-    "blood_pressure": 72,
-}
-dict_example.update({"glucose_concentration": 85})
-print(dict_example)
+if 5 <= a <= 10:
+    print("YES")
