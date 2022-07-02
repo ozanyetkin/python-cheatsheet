efe95a6378b101b3c2f5f88bfd21ae1db2bfe89d
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-07-02 19:34:41 +0300

    day 7 finished

--- 11_advent_of_code/day_7.py
+++ 11_advent_of_code/day_7.py
@@ -1,7 +1,7 @@
 import sys
 from collections import Counter
 
-sys.setrecursionlimit(1200)
+sys.setrecursionlimit(2000)
 
 with open('11_advent_of_code/day_7.txt') as f:
     line = f.readline()
@@ -53,7 +53,46 @@ def move_crabs(lst, count, step, dir=1):
 # print(align(positions, count_same(positions, 0, 1), -(count_same(positions, -1, -1) + 1)))
 
 pos_counts = dict(Counter(positions))
-pos_counts[min(pos_counts.keys())]
 
-def align(p_counts):
-    p_counts[min(p_counts.keys())]
+def align(p_counts, left, right, left_cost=0, right_cost=0):
+    if left == right:
+        return left
+
+    n_left_cost = fuel_calc(p_counts, left + 1, 1) - left_cost
+    n_right_cost = fuel_calc(p_counts, right - 1, -1) - right_cost
+
+    if n_left_cost <= n_right_cost:
+        left += 1
+        left_cost += n_left_cost
+    else:
+        right -= 1
+        right_cost += n_right_cost
+    return align(p_counts, left, right, left_cost, right_cost)
+
+def fuel_calc(p_counts, final, side):
+    fuel_cons = 0
+    p_keys = list(p_counts.keys())
+    p_keys.sort()
+    if side == 1:
+        for key in p_keys:
+            if key > final:
+                break
+            fuel_cons += p_counts[key] * int((final - key) * (final - key + 1) / 2)
+    else:
+        p_keys.reverse()
+        for key in p_keys:
+            if key < final:
+                break
+            fuel_cons += p_counts[key] * int((key - final) * (key - final + 1) / 2)
+    return fuel_cons
+
+test = [16,1,2,0,4,2,7,1,2,14]
+test = dict(Counter(test))
+# print(test)
+# print(fuel_calc(test, 5, -1))
+p_min = min(pos_counts.keys())
+p_max = max(pos_counts.keys())
+
+best = align(pos_counts, p_min, p_max)
+print(best)
+print(fuel_calc(pos_counts, best, 1) + fuel_calc(pos_counts, best, -1))
