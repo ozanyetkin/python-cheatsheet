e039a5e2f7b1b594766dd305900dd47a6b16c026
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-07-03 19:51:17 +0300

    day 9 finished

--- 11_advent_of_code/day_9.py
+++ 11_advent_of_code/day_9.py
@@ -27,7 +27,7 @@ for i in range(100):
         risk = True
         for neighbor in neighbors:
             try:
-                if map[i][j] >= map[neighbor[0]][neighbor[1]]:
+                if map[i][j] >= map[abs(neighbor[0])][abs(neighbor[1])]:
                     risk = False
             except:
                 pass
@@ -38,10 +38,40 @@ total = 0
 for risk_point in risk_points:
     total += map[risk_point[0]][risk_point[1]] + 1
 
-print(risk_points)
+print(total)
 
+def walker(basin_map, path=set(), area=0):
+    if len(path) == 0:
+        return area
+    pos = path.pop()
+    try:
+        if basin_map[pos[0]][pos[1]] != 9:
+            area += 1
+            basin_map[pos[0]][pos[1]] = 9
+    except:
+        pass
+    for neighbor in find_neighbor(pos):
+        try:
+            if basin_map[abs(neighbor[0])][abs(neighbor[1])] != 9:
+                path.add((neighbor))
+        except IndexError:
+            pass
+    return walker(basin_map, path, area)
+
+sizes = []
+for risk_point in risk_points:
+    sizes.append(walker(map, set([(risk_point)])))
+
+sizes.sort(reverse=True)
+answer = 1
+for i in range(3):
+    answer *= sizes[i]
+
+print(answer)
+"""
 fig, ax = plt.subplots()
 im = ax.imshow(map)
 for risk_point in risk_points:
     plt.scatter(risk_point[1], risk_point[0], c="red", s=0.5)
 plt.savefig("map.png")
+"""
