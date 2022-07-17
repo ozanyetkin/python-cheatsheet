0b2a450218311ea3b96bf7c768ce8808cd0a1c22
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-07-17 19:41:45 +0300

    day 12 stucked

--- 11_advent_of_code/day_12.py
+++ 11_advent_of_code/day_12.py
@@ -1,5 +1,7 @@
 import sys
-sys.setrecursionlimit(20000)
+from collections import Counter
+
+sys.setrecursionlimit(15000)
 
 with open("11_advent_of_code/day_12.txt") as f:
     lines = f.readlines()
@@ -14,7 +16,7 @@ for node in unique_nodes:
         if node in piece:
             val = val.union(piece.difference(set([node])))
     path_dict[node] = val
-
+"""
 def walker(path: dict, path_l=[], current_path=["start"], finished_path=[], current="start"):
     if current == "end":
         finished_path.append(current_path)
@@ -44,5 +46,43 @@ def walker(path: dict, path_l=[], current_path=["start"], finished_path=[], curr
             return walker(path, path_l, current_path, finished_path, current_path[-1])
         else:
             return finished_path
+"""
+# print(len(walker(path_dict)))
 
-print(len(walker(path_dict)))
+def walker(map: dict, chosen: str, current_path=[]):
+    optional_paths = []
+    options = option_finder(map, current_path, chosen) 
+    for option in options:
+        if option != "end":
+            optional_paths.append(current_path + [option])
+    if "end" in options:
+        finished_path = current_path + ["end"]
+        return finished_path, optional_paths, True
+    else:
+        return [], optional_paths, False
+
+
+def option_finder(map: dict, current_path: list, chosen: str):
+    counts = Counter(current_path)
+    forbidden = set(filter(lambda x: x.islower(), current_path))
+
+    if counts[chosen] < 2 and chosen in forbidden:
+        forbidden.remove(chosen)
+    return map[current_path[-1]].difference(forbidden)
+
+def collector(map: dict, chosen: str, unfinished_paths=[["start"]], finished_paths=[]):
+    if len(unfinished_paths) > 0:
+        finished_path, optional_paths, is_finished = walker(map, chosen, unfinished_paths.pop())
+        if is_finished:
+            finished_paths.append(finished_path)
+        unfinished_paths += optional_paths
+        return collector(map, chosen, unfinished_paths, finished_paths)
+    else:
+        return finished_paths
+
+chosen_set = set(filter(lambda x: x.islower() and x not in ["start", "end"], path_dict.keys()))
+all_paths = []
+for chosen in chosen_set:
+    all_paths += collector(path_dict, chosen)
+
+print(len(all_paths))
