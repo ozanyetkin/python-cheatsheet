155bb6c69e9b01f24dac6469e231bc64af254e17
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-08-14 17:42:52 +0300

    stuck at day 18

--- 11_advent_of_code/day_18.py
+++ 11_advent_of_code/day_18.py
@@ -2,9 +2,9 @@ from math import ceil
 
 
 class Snail:
-    def __init__(self, input):
-        self.depth_list = []
+    def __init__(self, input=str):
         self.char_list = []
+        self.depth_list = []
         depth = 0
         for char in input:
             if char.isnumeric():
@@ -15,10 +15,9 @@ class Snail:
             elif char == "]":
                 depth -= 1
         self.length = len(self.char_list)
-        self.reduce()
 
     def explode(self, i=0):
-        if self.depth_list[i] == 4:
+        if self.depth_list[i] >= 5:
             if i == 0:
                 self.char_list[i + 2] += self.char_list[i + 1]
             elif i == self.length - 2:
@@ -26,15 +25,13 @@ class Snail:
             else:
                 self.char_list[i + 2] += self.char_list[i + 1]
                 self.char_list[i - 1] += self.char_list[i]
-
             self.char_list[i + 1] = 0
             self.char_list.pop(i)
-            self.depth_list[i + 1] = 3
+            self.depth_list[i + 1] -= 1
             self.depth_list.pop(i)
             self.length -= 1
-            print(self.char_list)
-            self.explode(i=0)
-        elif i < self.length - 2:
+        if i < self.length - 2:
+            print(self.char_list, i)
             self.explode(i + 1)
 
     def split(self, i=0):
@@ -45,15 +42,29 @@ class Snail:
             self.depth_list[i] += 1
             self.depth_list.insert(i, self.depth_list[i])
             self.length += 1
-            print(self.char_list)
-            self.explode()
-            self.split()
-        elif i < self.length - 1:
+            if i < self.length - 2:
+                self.split(i + 2)
+        if i < self.length - 1:
+            print(self.char_list, i)
             self.split(i + 1)
 
     def reduce(self):
-        self.explode()
-        self.split()
+        while max(self.depth_list) >= 5:
+            self.explode()
+            self.split()
 
-s = Snail("[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]")
-print(s.char_list)
+    def __add__(self, other):
+        self.char_list += other.char_list
+        self.depth_list += other.depth_list
+        self.depth_list = [x + 1 for x in self.depth_list]
+        self.length += other.length
+        self.reduce()
+        return self
+
+
+s1 = Snail("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]")
+
+with open ("11_advent_of_code/day_18.txt") as f:
+    for line in f:
+        s1 = s1 + Snail(line[:-1])
+        print(s1.char_list)
