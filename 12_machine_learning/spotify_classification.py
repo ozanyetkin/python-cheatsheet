804ef3ba1eaf3b9ba058041fcebf8ed909150a5b
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-10-02 15:59:49 +0300

    spotify classification updated

--- 12_machine_learning/spotify_classification.py
+++ 12_machine_learning/spotify_classification.py
@@ -5,7 +5,15 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.metrics import confusion_matrix
 
 df = pd.read_csv("SpotifyFeatures.csv")
@@ -13,7 +21,7 @@ df = df.drop(columns=["artist_name", "track_name", "track_id"])
 
 enc = OneHotEncoder(sparse=False, dtype=int)
 
-y_data = enc.fit_transform(df[["genre"]])
+y_data = pd.get_dummies(df["genre"])
 x_data = df.drop(columns=["genre"])
 
 x_cat_data = enc.fit_transform(x_data[["key", "mode", "time_signature"]])
@@ -28,18 +36,17 @@ x_data = scaler.transform(x_data)
 # x_data = normalize(x_data, norm="max", axis=0)
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size=0.7)
 
-neigh = KNeighborsClassifier(n_neighbors=3)
-neigh.fit(x_train, y_train)
+classifier = MLPClassifier(max_iter=10000)
 
-# TODO: https://stackoverflow.com/questions/38334296/reversing-one-hot-encoding-in-pandas
+classifier.fit(x_train, y_train)
 
-y_pred = neigh.predict(x_test)
-y_test = pd.DataFrame(y_test).idxmax(axis=1)
-y_pred = pd.DataFrame(y_pred).idxmax(axis=1)
+y_pred = classifier.predict(x_test)
+y_pred = pd.DataFrame(y_pred, columns=y_test.columns).idxmax(axis=1)
+y_test = y_test.idxmax(axis=1)
 cm = confusion_matrix(y_test, y_pred)
 
 df_cm = pd.DataFrame(cm, range(27), range(27))
-sn.set(font_scale=1.4)
-sn.heatmap(df_cm, annot=True, annot_kws={"size": 27})
+sn.set(font_scale=0.4)
+sn.heatmap(df_cm, annot=True)
 
 plt.show()
